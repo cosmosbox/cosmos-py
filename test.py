1a54d9f94b34c7ca2755f33e4c480de15c2141c2
Mrkelly <chepy.v@gmail.com>
2015-07-23 18:14:44 +0800

    patch all

--- test.py
+++ test.py
@@ -1,20 +1,23 @@
 import gevent
-from gevent import thread
 from gevent import monkey
-#monkey.patch_thread()
+monkey.patch_all()
 
 from gevent import Greenlet
 import etcd
 import random
+import threading
 
 client = etcd.Client()
 
 def watchTask():
 	print 'now watch...'
+	def do():
+		rVal = client.read('/nodes/abc', wait=True)
+		print "!!!!!!!!!!!!!!!!!!!!!ChangeVal %s" % rVal
 
-	rVal = client.read('/nodes/abc', wait=True)
-	print "!!!!!!!!!!!!!!!!!!!!!ChangeVal %s" % rVal
-
+	t = threading.Thread(target=do)
+	t.start()
+	
 	print 'now watch2...'
 
 	print 'watch finished...'
@@ -22,12 +25,13 @@ def watchTask():
 def task1():
 	def asyncSet():
 		setVal = random.randint(0,99)
+		print 'real set - %s' % setVal
 		client.set('/nodes/abc', setVal)
 		print 'set ok - %s' % setVal
 
 	for i in range(5):
 		print 'now set...'
-		Greenlet.spawn(asyncSet).start()
+		Greenlet.spawn(asyncSet).join()
 	print('Finished!!!!!!!')
 
 def task2():
@@ -37,16 +41,17 @@ def task2():
 
 	for i in range(5):
 		print 'now get....'
-		Greenlet.spawn(asyncGet).start()
+		Greenlet.spawn(asyncGet).join()
 	print('Finished!!!!!!! Task2')
 
 
-thread.start_new_thread(watchTask)
+
+gevent.sleep(1)
 
 gevent.joinall([
-	
-
+	gevent.spawn(watchTask),
 	gevent.spawn(task1),
 	gevent.spawn(task2)
+
 	
 ])
